f6080bc825dad56fd828b5ddf5ab75cc18112833
With Creative <withcreative81@gmail.com>
2018-09-15 21:35:02 +0300

    Update Cold.vb
    Updated to v3

--- Cold.vb
+++ Cold.vb
@@ -1,9 +1,9 @@
 '<------------------>
 'Author: WithCreative
 'Created: 12.12.2017
-'Updated: 13.12.2017
-'Category: Game Hacking
+'Updated: 15.09.2018
 'Description: Cold Hack D3D9 Menu UI
+'Version: v3
 '<------------------>
 
 Imports System.Drawing
@@ -12,6 +12,7 @@ Imports System.Runtime.InteropServices
 Imports System.IO
 Imports System.Reflection
 Imports System.ComponentModel
+Imports System.Drawing.Text
 
 Public Enum MouseState As Byte
     None = 0
@@ -107,6 +108,341 @@ Class ColdForm
         MyBase.OnLocationChanged(e)
     End Sub
 End Class
+#Region " RoundRectangle "
+
+Module RoundRectangle
+    Public Function RoundRect(ByVal Rectangle As Rectangle, ByVal Curve As Integer) As GraphicsPath
+        Dim P As GraphicsPath = New GraphicsPath()
+        Dim ArcRectangleWidth As Integer = Curve * 2
+        P.AddArc(New Rectangle(Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -180, 90)
+        P.AddArc(New Rectangle(Rectangle.Width - ArcRectangleWidth + Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -90, 90)
+        P.AddArc(New Rectangle(Rectangle.Width - ArcRectangleWidth + Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), 0, 90)
+        P.AddArc(New Rectangle(Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), 90, 90)
+        P.AddLine(New Point(Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y), New Point(Rectangle.X, Curve + Rectangle.Y))
+        Return P
+    End Function
+    Public Function RoundRect(ByVal X As Integer, ByVal Y As Integer, ByVal Width As Integer, ByVal Height As Integer, ByVal Curve As Integer) As GraphicsPath
+        Dim Rectangle As Rectangle = New Rectangle(X, Y, Width, Height)
+        Dim P As GraphicsPath = New GraphicsPath()
+        Dim ArcRectangleWidth As Integer = Curve * 2
+        P.AddArc(New Rectangle(Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -180, 90)
+        P.AddArc(New Rectangle(Rectangle.Width - ArcRectangleWidth + Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -90, 90)
+        P.AddArc(New Rectangle(Rectangle.Width - ArcRectangleWidth + Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), 0, 90)
+        P.AddArc(New Rectangle(Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), 90, 90)
+        P.AddLine(New Point(Rectangle.X, Rectangle.Height - ArcRectangleWidth + Rectangle.Y), New Point(Rectangle.X, Curve + Rectangle.Y))
+        Return P
+    End Function
+    Public Function RoundedTopRect(ByVal Rectangle As Rectangle, ByVal Curve As Integer) As GraphicsPath
+        Dim P As GraphicsPath = New GraphicsPath()
+        Dim ArcRectangleWidth As Integer = Curve * 2
+        P.AddArc(New Rectangle(Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -180, 90)
+        P.AddArc(New Rectangle(Rectangle.Width - ArcRectangleWidth + Rectangle.X, Rectangle.Y, ArcRectangleWidth, ArcRectangleWidth), -90, 90)
+        P.AddLine(New Point(Rectangle.X + Rectangle.Width, Rectangle.Y + ArcRectangleWidth), New Point(Rectangle.X + Rectangle.Width, Rectangle.Y + Rectangle.Height - 1))
+        P.AddLine(New Point(Rectangle.X, Rectangle.Height - 1 + Rectangle.Y), New Point(Rectangle.X, Rectangle.Y + Curve))
+        Return P
+    End Function
+End Module
+
+#End Region
+#Region " TrackBar "
+
+<DefaultEvent("ValueChanged")> Class ColdTrackBar
+    Inherits Control
+
+#Region " Enums "
+
+    Enum ValueDivisor
+        By1 = 1
+        By10 = 10
+        By100 = 100
+        By1000 = 1000
+    End Enum
+
+#End Region
+#Region " Variables "
+
+    Private PipeBorder, FillValue As GraphicsPath
+    Private TrackBarHandleRect As Rectangle
+    Private Cap As Boolean
+    Private ValueDrawer As Integer
+
+    Private ThumbSize As Size = New Size(15, 15)
+    Private TrackThumb As Rectangle
+
+    Private _Minimum As Integer = 0
+    Private _Maximum As Integer = 10
+    Private _Value As Integer = 0
+
+    Private _DrawValueString As Boolean = False
+    Private _JumpToMouse As Boolean = False
+    Private DividedValue As ValueDivisor = ValueDivisor.By1
+
+#End Region
+#Region " Properties "
+
+    Public Property Minimum() As Integer
+        Get
+            Return _Minimum
+        End Get
+        Set(ByVal value As Integer)
+
+            If value >= _Maximum Then value = _Maximum - 10
+            If _Value < value Then _Value = value
+
+            _Minimum = value
+            Invalidate()
+        End Set
+    End Property
+
+    Public Property Maximum() As Integer
+        Get
+            Return _Maximum
+        End Get
+        Set(ByVal value As Integer)
+
+            If value <= _Minimum Then value = _Minimum + 10
+            If _Value > value Then _Value = value
+
+            _Maximum = value
+            Invalidate()
+        End Set
+    End Property
+
+    Event ValueChanged()
+    Public Property Value() As Integer
+        Get
+            Return _Value
+        End Get
+        Set(ByVal value As Integer)
+            If _Value <> value Then
+                If value < _Minimum Then
+                    _Value = _Minimum
+                Else
+                    If value > _Maximum Then
+                        _Value = _Maximum
+                    Else
+                        _Value = value
+                    End If
+                End If
+                Invalidate()
+                RaiseEvent ValueChanged()
+            End If
+        End Set
+    End Property
+
+    Public Property ValueDivison() As ValueDivisor
+        Get
+            Return DividedValue
+        End Get
+        Set(ByVal Value As ValueDivisor)
+            DividedValue = Value
+            Invalidate()
+        End Set
+    End Property
+
+    <Browsable(False)> Public Property ValueToSet() As Single
+        Get
+            Return CSng(_Value / DividedValue)
+        End Get
+        Set(ByVal Val As Single)
+            Value = CInt(Val * DividedValue)
+        End Set
+    End Property
+
+    Public Property JumpToMouse() As Boolean
+        Get
+            Return _JumpToMouse
+        End Get
+        Set(ByVal value As Boolean)
+            _JumpToMouse = value
+            Invalidate()
+        End Set
+    End Property
+
+    Property DrawValueString() As Boolean
+        Get
+            Return _DrawValueString
+        End Get
+        Set(ByVal value As Boolean)
+            _DrawValueString = value
+            If _DrawValueString = True Then
+                Height = 35
+            Else
+                Height = 22
+            End If
+            Invalidate()
+        End Set
+    End Property
+
+#End Region
+#Region " EventArgs "
+
+    Protected Overrides Sub OnMouseMove(ByVal e As MouseEventArgs)
+        MyBase.OnMouseMove(e)
+        If Cap = True AndAlso e.X > -1 AndAlso e.X < (Width + 1) Then
+            Value = _Minimum + CInt((_Maximum - _Minimum) * (e.X / Width))
+        End If
+    End Sub
+
+    Protected Overrides Sub OnMouseDown(ByVal e As MouseEventArgs)
+        MyBase.OnMouseDown(e)
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ValueDrawer = CInt((_Value - _Minimum) / (_Maximum - _Minimum) * (Width - 11))
+            TrackBarHandleRect = New Rectangle(ValueDrawer, 0, 25, 25)
+            Cap = TrackBarHandleRect.Contains(e.Location)
+            Focus()
+            If _JumpToMouse Then
+                Value = _Minimum + CInt((_Maximum - _Minimum) * (e.X / Width))
+            End If
+        End If
+    End Sub
+
+    Protected Overrides Sub OnMouseUp(ByVal e As MouseEventArgs)
+        MyBase.OnMouseUp(e)
+        Cap = False
+    End Sub
+
+#End Region
+
+    Sub New()
+        SetStyle(ControlStyles.AllPaintingInWmPaint Or _
+         ControlStyles.UserPaint Or _
+         ControlStyles.ResizeRedraw Or _
+         ControlStyles.DoubleBuffer, True)
+        Size = New Size(80, 22)
+        MinimumSize = New Size(47, 22)
+    End Sub
+
+    Protected Overrides Sub OnResize(e As EventArgs)
+        MyBase.OnResize(e)
+        If _DrawValueString = True Then
+            Height = 35
+        Else
+            Height = 22
+        End If
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
+        Dim G As Graphics = e.Graphics
+
+        G.Clear(Color.FromArgb(218, 218, 218))
+        G.SmoothingMode = SmoothingMode.AntiAlias
+        TrackThumb = New Rectangle(8, 8, Width - 11, 2)
+        PipeBorder = RoundRectangle.RoundRect(1, 8, Width - 3, 1, 1)
+
+        Try
+            ValueDrawer = CInt((_Value - _Minimum) / (_Maximum - _Minimum) * (Width - 11))
+        Catch ex As Exception
+        End Try
+
+        TrackBarHandleRect = New Rectangle(ValueDrawer, 0, 10, 20)
+
+        G.SetClip(PipeBorder)
+        G.FillPath(New SolidBrush(Color.FromArgb(221, 221, 221)), PipeBorder)
+        FillValue = RoundRectangle.RoundRect(0, 8, TrackBarHandleRect.X + TrackBarHandleRect.Width - 3, 2, 1)
+
+        G.ResetClip()
+
+        G.SmoothingMode = SmoothingMode.HighQuality
+        G.DrawPath(New Pen(Color.FromArgb(0, 0, 0)), PipeBorder)
+        ' G.FillPath(Brushes.DarkGray, FillValue)
+
+        G.DrawRectangle(Pens.Black, TrackThumb.X + CInt(TrackThumb.Width * (Value / Maximum)) - CInt(ThumbSize.Width / 2), TrackThumb.Y + CInt((TrackThumb.Height / 2)) - CInt(ThumbSize.Height / 2), ThumbSize.Width - 6, ThumbSize.Height)
+        G.FillRectangle(DrawGradient(ThumbSize.Width, ThumbSize.Height), TrackThumb.X + CInt(TrackThumb.Width * (Value / Maximum)) - CInt(ThumbSize.Width / 2), TrackThumb.Y + CInt((TrackThumb.Height / 2)) - CInt(ThumbSize.Height / 2), ThumbSize.Width - 6, ThumbSize.Height)
+
+        If _DrawValueString = True Then
+            G.DrawString(ValueToSet, Font, Brushes.DimGray, 1, 20)
+        End If
+    End Sub
+End Class
+
+#End Region
+Class ColdListBox
+    Inherits ListBox
+    Public Function RoundRect(ByVal rectangle As Rectangle, ByVal curve As Integer) As GraphicsPath
+        Dim p As GraphicsPath = New GraphicsPath()
+        Dim arcRectangleWidth As Integer = curve * 1
+        p.AddArc(New Rectangle(rectangle.X, rectangle.Y, arcRectangleWidth, arcRectangleWidth), -180, 90)
+        p.AddArc(New Rectangle(rectangle.Width - arcRectangleWidth + rectangle.X, rectangle.Y, arcRectangleWidth, arcRectangleWidth), -90, 90)
+        p.AddArc(New Rectangle(rectangle.Width - arcRectangleWidth + rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y, arcRectangleWidth, arcRectangleWidth), 0, 90)
+        p.AddArc(New Rectangle(rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y, arcRectangleWidth, arcRectangleWidth), 90, 90)
+        p.AddLine(New Point(rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y), New Point(rectangle.X, curve + rectangle.Y))
+        Return p
+    End Function
+    Public Function RoundRect(ByVal x As Integer, ByVal y As Integer, ByVal width As Integer, ByVal height As Integer, ByVal curve As Integer) As GraphicsPath
+        Dim rectangle As Rectangle = New Rectangle(x, y, width, height)
+        Dim p As GraphicsPath = New GraphicsPath()
+        Dim arcRectangleWidth As Integer = curve * 1
+        p.AddArc(New Rectangle(rectangle.X, rectangle.Y, arcRectangleWidth, arcRectangleWidth), -180, 90)
+        p.AddArc(New Rectangle(rectangle.Width - arcRectangleWidth + rectangle.X, rectangle.Y, arcRectangleWidth, arcRectangleWidth), -90, 90)
+        p.AddArc(New Rectangle(rectangle.Width - arcRectangleWidth + rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y, arcRectangleWidth, arcRectangleWidth), 0, 90)
+        p.AddArc(New Rectangle(rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y, arcRectangleWidth, arcRectangleWidth), 90, 90)
+        p.AddLine(New Point(rectangle.X, rectangle.Height - arcRectangleWidth + rectangle.Y), New Point(rectangle.X, curve + rectangle.Y))
+        Return p
+    End Function
+    Private mFont As New Font("Segoe UI", 8)
+    Protected Overrides Sub OnCreateControl()
+        Dock = DockStyle.None
+        Invalidate()
+    End Sub
+    Sub New()
+        SetStyle(ControlStyles.AllPaintingInWmPaint Or ControlStyles.OptimizedDoubleBuffer Or _
+                 ControlStyles.SupportsTransparentBackColor, True)
+        DoubleBuffered = True
+        DrawMode = Windows.Forms.DrawMode.OwnerDrawFixed
+        BackColor = Color.FromArgb(246, 246, 246)
+        Width = 200
+        Height = 200
+        Me.Font = mFont
+        Me.ForeColor = Color.Black
+        Me.ItemHeight = 20
+        Me.BorderStyle = Windows.Forms.BorderStyle.FixedSingle
+        Me.ScrollAlwaysVisible = False
+        Me.HorizontalScrollbar = False
+        Me.IntegralHeight = False
+    End Sub
+    Protected Overrides Sub OnPaint(e As System.Windows.Forms.PaintEventArgs)
+        Using g = e.Graphics
+            g.SmoothingMode = SmoothingMode.HighQuality
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality
+            g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit
+            g.Clear(Color.FromArgb(246, 246, 246))
+            g.DrawLine(Pens.Black, New Point(0, 0), New Point(Me.Width, 0))
+            g.DrawLine(Pens.Black, New Point(0, Me.Height), New Point(0, 0))
+            g.DrawLine(Pens.Black, New Point(Width + 1, 0), New Point(Width + 1, Me.Height))
+            g.DrawLine(Pens.Black, New Point(0, Me.Height + 1), New Point(Me.Width, Me.Height + 1))
+            g.Dispose()
+        End Using
+        MyBase.OnPaint(e)
+        e.Dispose()
+        e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic
+    End Sub
+    Protected Overrides Sub OnDrawItem(e As DrawItemEventArgs)
+        Using g = e.Graphics
+            e.DrawBackground()
+            e.DrawFocusRectangle()
+            g.SmoothingMode = SmoothingMode.HighQuality
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic
+            g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit
+            g.FillRectangle(New SolidBrush(BackColor), New Rectangle(e.Bounds.X, e.Bounds.Y - 1, e.Bounds.Width, e.Bounds.Height + 3))
+            If e.State.ToString().Contains("Selected,") Then
+                Dim selectgb As New SolidBrush(Color.FromArgb(217, 219, 218))
+                g.FillRectangle(selectgb, New Rectangle(e.Bounds.X, e.Bounds.Y + 1, e.Bounds.Width, e.Bounds.Height))
+                g.DrawRectangle(New Pen(Color.FromArgb(212, 212, 212)), New Rectangle(e.Bounds.X, e.Bounds.Y + 1, e.Bounds.Width, e.Bounds.Height))
+            Else
+                Dim nonselectgb As New SolidBrush(Color.FromArgb(246, 246, 246))
+                g.FillRectangle(nonselectgb, e.Bounds)
+            End If
+            Try
+                g.DrawString(Items(e.Index).ToString(), mFont, New SolidBrush(Color.FromArgb(0, 0, 0)), New Rectangle(e.Bounds.X + 3, e.Bounds.Y + 5, e.Bounds.Width, e.Bounds.Height), New StringFormat With {.LineAlignment = StringAlignment.Near, .Alignment = StringAlignment.Near, .FormatFlags = StringFormatFlags.FitBlackBox})
+            Catch : End Try
+
+            g.Dispose()
+        End Using
+    End Sub
+End Class
 Class ColdButton
     Inherits Control
     Dim Down As Boolean = False
@@ -114,8 +450,8 @@ Class ColdButton
     Dim MousePoint As New Point(0, 0)
 #Region "MouseStates"
     Protected Overrides Sub OnMouseDown(e As MouseEventArgs)
-            isMousePress = True
-            Me.Refresh()
+        isMousePress = True
+        Me.Refresh()
     End Sub
     Protected Overrides Sub OnMouseUp(e As MouseEventArgs)
         isMousePress = False
@@ -436,10 +772,19 @@ End Class
         MyBase.OnCreateControl()
     End Sub
     Sub New()
-        SetStyle(ControlStyles.AllPaintingInWmPaint, True)
         BorderStyle = BorderStyle.FixedSingle
-        BackColor = Color.FromArgb(245, 245, 245)
+        BackColor = Color.FromArgb(216, 216, 216)
         Margin = New Padding(5, 0, 0, 0)
+        Font = New Font("Segoe UI", 8)
+    End Sub
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        Using g = e.Graphics
+            g.DrawLine(Pens.Black, New Point(0, 0), New Point(Me.Width, 0)) 'Top
+            g.DrawLine(Pens.Black, New Point(0, Me.Height), New Point(0, 0)) 'Left
+            g.DrawLine(Pens.Black, New Point(Width - 1, 0), New Point(Width - 1, Me.Height)) 'Right
+            g.DrawLine(Pens.Black, New Point(0, Me.Height - 1), New Point(Me.Width, Me.Height - 1)) 'Bottom
+        End Using
+        MyBase.OnPaint(e)
     End Sub
 End Class
 Class ColdProgressBar
@@ -495,21 +840,28 @@ Class ColdProgressBar
         Size = New Size(100, 24)
         Maximum = 100
         Minimum = 0
+        Text = ""
+        Font = New Font("Segoe UI", 8)
     End Sub
     Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
         MyBase.OnPaint(e)
+        Dim _text As String = Text
+        Text = ""
         Dim G As Graphics = e.Graphics
         G.SmoothingMode = SmoothingMode.HighQuality
         G.Clear(Parent.BackColor)
         Dim mainRect As New Rectangle(0, 0, Width - 1, Height - 1)
-        G.FillRectangle(New SolidBrush(Color.FromArgb(240, 240, 240)), mainRect)
+        G.FillRectangle(New SolidBrush(Color.FromArgb(235, 235, 235)), mainRect)
         G.DrawRectangle(Pens.Black, mainRect)
         Dim barRect As New Rectangle(0, 0, CInt(((Width / _Maximum) * _Value) - 1), Height - 1)
         G.FillRectangle(DrawGradient(Height, CInt(((Width / _Maximum) * _Value) - 1)), barRect)
         Dim percent As Single = (_Value / _Maximum) * 100
         Dim txt As String = Text & " " & CStr(percent) & "%"
         If ShowPercant Then
-            G.DrawString(txt, Font, Brushes.Black, LeftText(Text, Font, Width, Height / 2 + 2))
+            G.DrawString(txt, Font, Brushes.Black, LeftText(Text, Font, Width, Height / 2 - 1))
+        Else
+            G.DrawString(_text, Font, Brushes.Black, CenterText(Text, Font, Width, Height / 2 - 3))
         End If
+        Text = _text
     End Sub
 End Class
